f91518a5e16d0baf1a95f48c37e2305129a2ffd6
David <davidmwathi412@gmail.com>
2024-08-07 11:05:06 +0300

    Added changes

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,7 +3,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Header from './Header'
+import MainContent from './MainContent'
+import Footer from './Footer'
 
 
 
