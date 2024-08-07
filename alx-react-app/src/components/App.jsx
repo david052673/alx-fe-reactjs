c04eca86f2a338411a8e23dad0e1f8fe01d3f58b
David <davidmwathi412@gmail.com>
2024-08-07 16:53:44 +0300

    Added changes

--- alx-react-app/src/components/App.jsx
+++ alx-react-app/src/components/App.jsx
@@ -6,7 +6,7 @@ import WelcomeMessage from './WelcomeMessage'
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
-
+import UserProfile from './UserProfile'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -37,6 +37,7 @@ function App() {
       <MainContent />
       <Footer />
       <Header />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </>
   )
 }
