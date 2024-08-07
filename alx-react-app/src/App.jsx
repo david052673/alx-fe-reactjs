1826e4a38e8a811201ed045d7f138ae61a274df3
David <davidmwathi412@gmail.com>
2024-08-07 10:51:48 +0300

    Added changes

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 
 import Header from './Header'
 import MainContent from './MainContent'
-import Footer from './Footer'
+
 
 function App() {
   const [count, setCount] = useState(0)
