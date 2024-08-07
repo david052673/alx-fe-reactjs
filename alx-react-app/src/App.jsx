e596488e616f982a8972ba9f17e4491e995f2abe
David <davidmwathi412@gmail.com>
2024-08-07 16:55:48 +0300

    Added changes

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import WelcomeMessage from './WelcomeMessage'
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
+import UserProfile from './UserProfile'
 
 
 function App() {
@@ -37,6 +38,7 @@ function App() {
       <MainContent />
       <Footer />
       <Header />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </>
   )
 }
