2a3169928b9e6e0bdcbe56f9ff97526a09f00da7
David <davidmwathi412@gmail.com>
2024-08-07 10:54:31 +0300

    Added changes

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,8 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import Header from './Header'
-import MainContent from './MainContent'
+
 
 
 function App() {
