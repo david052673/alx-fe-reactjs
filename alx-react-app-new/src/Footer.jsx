44195d926869e04480a55000da60da3c9adb1dcb
David <davidmwathi412@gmail.com>
2024-08-12 17:23:47 +0300

    Added changes

---
+++ alx-react-app-new/src/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+        <footer>
+            <p> style={{color:'blue'}}© 2023 City Lovers</p>
+        </footer>
+
+    );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/MainContent.jsx
@@ -0,0 +1,12 @@
+import { buildErrorMessage } from "vite";
+
+function MainContent() {
+    return (
+        <main>
+            <p> style={{color:'blue', background:'black'}}I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+
+    );
+}
+
+export default MainContent;
