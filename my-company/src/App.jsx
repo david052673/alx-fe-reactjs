bafa3e2fcd6ea59e09ec01fcf7d1e5a976993f19
David <davidmwathi412@gmail.com>
2024-08-13 16:44:51 +0300

    Added changes

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -31,5 +31,26 @@ function App() {
     </>
   )
 }
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import Service from './Service';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/service" element={<Service />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+
 
 export default App
