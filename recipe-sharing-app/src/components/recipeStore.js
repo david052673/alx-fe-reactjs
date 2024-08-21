6210ba8731f09e84da0b6e200dd53103d15564c6
David <davidmwathi412@gmail.com>
2024-08-21 08:37:38 +0300

    Added changes

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -6,4 +6,11 @@ const useRecipeStore = create(set => ({
   setRecipes: (recipes) => set({ recipes })
   updateRecipe:(updateRecipe) => set(state =>({recipes: { ...state.recipes,newRecipe} })),
   deleteRecipe :(deleteRecipe) => set(state =>({recipes: { ...state.recipes,newRecipe} })),
-}));
+  searchTerm: '',
+  setSearchTerm: (term) => set({ searchTerm: term }),
+  filteredRecipes: [],
+  filterRecipes: () => set(state => ({
+    filteredRecipes: state.recipes.filter(recipe =>
+      recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+    ))
+})),
