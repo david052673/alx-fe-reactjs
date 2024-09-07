ab3417af06f0955cda922332212992a02933a290
David <davidmwathi412@gmail.com>
2024-09-07 15:45:27 +0300

    Added changes

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './Todolist.jsx'
+import TodoLIst from './components.jsx/TodoList';
 
 
 function App() {
@@ -33,7 +33,7 @@ function App() {
       </p>
     </>
   )
-  </Todolist.jsx>
+  </Todolist>
 }
 
 export default App
