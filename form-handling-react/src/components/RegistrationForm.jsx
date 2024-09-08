74f3068691c9f7a3dca5c9a05250e94b6222c1f2
David <davidmwathi412@gmail.com>
2024-09-08 20:48:05 +0300

    Added changes

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -29,7 +29,7 @@ const RegistrationForm = () => {
                         type="text"
                         id="username"
                         name="username"
-                        value={"username"}
+                        value={username}
                         onChange={handleChange}
                         required
                     />
@@ -40,7 +40,7 @@ const RegistrationForm = () => {
                         type="email"
                         id="email"
                         name="email"
-                        value={"email"}
+                        value={email}
                         onChange={handleChange}
                         required
                     />
@@ -51,7 +51,7 @@ const RegistrationForm = () => {
                         type="password"
                         id="password"
                         name="password"
-                        value={"password"}
+                        value={password}
                         onChange={handleChange}
                         required
                     />
